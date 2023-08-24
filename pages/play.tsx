8e46d952e6c19d087d045a6dcc5065caa934d47d
Ankit Singh <ankit.singh.zm@gmail.com>
2023-08-25 00:28:56 +0530

    structured components

---
+++ pages/play.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function play() {
+    return (
+        <div className="flex flex-col items-center justify-center h-5/6  border-2 border-gray-800">
+            play gmae here
+        </div>
+    )
+}
+
+export default play
